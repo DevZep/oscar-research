a801243425642c608552040f9dc41b1795ca73d0
JANE LY <chimkanitha@gmail.com>
2019-01-24 10:39:50 +0700

    change application.rb module to ResearchModuleApp

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ require 'apartment/elevators/subdomain'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module OscarGovKh
+module ResearchModuleApp
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
